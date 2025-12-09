00a72377e52066e71fd9b39cae5fb19b2d2d21cb
Mattie Nash <mnashkittycat@gmail.com>
2025-12-09 11:12:32 -0600

    Remove custom core, since changes were upstreamed(?)

--- flake.nix
+++ flake.nix
@@ -4,31 +4,22 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    mupen64plus-core-custom.url = "github:JoshuaEagles/mupen64plus-libretro-nx";
-    mupen64plus-core-custom.flake = false;
     client.url = "path:./client";
   };
 
-  outputs = { self, nixpkgs, flake-utils, mupen64plus-core-custom, client }: flake-utils.lib.eachDefaultSystem (system:
+  outputs = { self, nixpkgs, flake-utils, client }: flake-utils.lib.eachDefaultSystem (system:
     let
       pkgs = import nixpkgs {
         inherit system;
       };
 
-      mupen64plus = pkgs.libretro.mupen64plus.overrideAttrs (old: {
-        src = mupen64plus-core-custom;
-      });
-
      myRetroarch = pkgs.wrapRetroArch {
-    cores = [ mupen64plus ];
-    settings = {
-      network_cmd_enable = "true";
-      # other config values here
-    };
-  };
-        
-        #pkgs.retroarch.withCores (cores: [ mupen64plus ]);
-
+       cores = [ pkgs.libretro.mupen64plus ];
+       settings = {
+         network_cmd_enable = "true";
+         # other config values here
+       };
+     };
     in
     {
       packages = {
